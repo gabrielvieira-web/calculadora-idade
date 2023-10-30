3d4af746430c0d74838a977a23b3bd27f7660977
gabrielvieira-web <vieira.github@gmail.com>
2023-10-30 17:37:05 -0300

    Lógica de calcular idade e exibir concluida

--- script.js
+++ script.js
@@ -1,34 +1,83 @@
 const inputDay = document.querySelector('#day');
 const inputMonth = document.querySelector('#month');
 const inputYear = document.querySelector('#year');
-let messageError = document.querySelectorAll('#messageError');
 const button = document.querySelector('#button');
+let messageError = document.querySelectorAll('#messageError');
+let spanYears = document.querySelector('#years');
+let spanMonths = document.querySelector('#months');
+let spanDays = document.querySelector('#days');
 
 button.addEventListener('click', (event) => {
   event.preventDefault();
-  validateDataForm();
+  const formValid = validateDataForm();
+  if(formValid) {
+    const age = calculateAge();
+    displayAge(age);
+  } 
 })
 
 function validateDataForm() {
-  validateInputDay();
-  validateInputMonth();
-  validateInputYear();
-  hasDayInMonth();
+  const dayValid = validateInputDay();
+  const monthValid = validateInputMonth();
+  const yearValid = validateInputYear();
+  const dayInMonthValid = hasDayInMonth();
+  if(dayValid && monthValid && yearValid && dayInMonthValid) {
+    return true;
+  }
+  return false;
+}
+
+function calculateAge() {
+  const currentDate = new Date();
+  const yearCurrent = currentDate.getFullYear();
+  const monthCurrent = currentDate.getMonth() + 1;
+  const dayCurrent = currentDate.getDate();
+
+  let years;
+  let months;
+  let days;
+
+  if(inputDay.value > dayCurrent || inputMonth.value > monthCurrent) {
+    years = (yearCurrent - Number(inputYear.value)) - 1;
+    months = 12 - (Number(inputMonth.value) - monthCurrent);
+    const dayCalculated = inputDay.value > dayCurrent ? dayCurrent - 1 : dayCurrent - inputDay.value;
+    days = dayCalculated;
+  } else {
+    years = yearCurrent - Number(inputYear.value);
+    months = monthCurrent - Number(inputMonth.value);
+    days = dayCurrent - Number(inputDay.value);
+  }
+
+  return {
+    years,
+    months,
+    days
+  }
+}
+
+function displayAge(age) {
+  spanYears.textContent = age.years;
+  spanMonths.textContent = age.months;
+  spanDays.textContent = age.days;
 }
 
 function validateInputDay() {
   if(inputDay.value === '' || inputDay.value.length > 2 || inputDay.value <= 0) {
     errorMessage(0);
+    return false;
   } else{
     messageError[0].textContent = "";
+    return true;
   }
 }
 
 function validateInputMonth() {
   if(inputMonth.value === '' || inputMonth.value.length > 2 || inputMonth.value <= 0 || inputMonth.value > 12) {
     errorMessage(1);
+    return false;
   } else{
     messageError[1].textContent = "";
+    return true;
   }
 }
 
@@ -36,21 +85,27 @@ function validateInputYear() {
   const currentYear = new Date().getFullYear();
   if(inputYear.value === '' || inputYear.value.length !== 4 || inputYear.value <= 0 || inputYear.value > currentYear) {
     errorMessage(2);
+    return false;
   } else{
     messageError[2].textContent = "";
+    return true;
   }
 }
 
 function hasDayInMonth() {
   // Verifica se o dia passado no input, existe no mês. ex: 31/04/2023 (DATA INVÁLIDA) pois não existe dia 31 no mês de Abril
-  const dateForm = new Date(`${inputYear.value}, ${inputMonth.value}, ${inputDay.value}`);
-  // Os números dos meses retornados pela função getMonth() vão de 0 á 11. sendo 0 JANEIRO e 11 
-  const numberNextMonth = dateForm.getMonth();
-  if(numberNextMonth == inputMonth.value) {
+  const lastDayMonth = new Date(inputYear.value, inputMonth.value, 0);
+
+  if(inputDay.value > lastDayMonth.getDate()) {
     errorMessage(0);
+    return false;
   }
+  return true;
 }
 
 function errorMessage(position) {
   messageError[position].textContent = "Este campo é obrigátorio";
+  spanYears.textContent = '- -';
+  spanMonths.textContent = '- -';
+  spanDays.textContent = '- -';
 }
